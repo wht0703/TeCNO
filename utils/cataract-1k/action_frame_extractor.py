83e2f36a2c7f0b54d96cf9d25633d029b69e7eef
Haotong Wang <haotong.wang@tum.de>
2024-11-16 15:34:23 +0100

    Fix typo in action_frame_extractor.py

--- utils/cataract-1k/action_frame_extractor.py
+++ utils/cataract-1k/action_frame_extractor.py
@@ -26,9 +26,9 @@ def extract_action_frames(video_path: str = '../../videos', annotation_path: str
     case_list.sort()
 
     print(f'Start extracting action frames')
-    outer  = tqdm(total=1, desc='Extracting action frames from: ', position=0, leave=True)
+    outer  = tqdm(total=3, desc='Extracting action frames from: ', position=0, leave=True)
 
-    for i in range(2, 3):
+    for i in range(0, 3):
         case_folder = os.path.join(annotation_path, case_list[i])
         video_info_csv = os.path.join(case_folder, f"{case_list[i]}_video.csv")
         video_annotation_csv = os.path.join(case_folder, f"{case_list[i]}_annotations_phases.csv")
@@ -50,7 +50,7 @@ def extract_action_frames(video_path: str = '../../videos', annotation_path: str
             if last_frame != annotations[j][0]:
                 frame_start = last_frame
                 frame_end = annotations[j][0] - 1
-                video_name = f"{seg_video}/{case_list[i]}_idle_{frame_start}-{frame_end}.mp4"
+                video_name = f"{seg_video}/{case_list[i]}_Idle_{frame_start}-{frame_end}.mp4"
                 convert_frames_to_videos(video, frame_start, frame_end, case_folder, video_name)
             frame_start = annotations[j][0]
             frame_end = annotations[j][1]
@@ -59,7 +59,7 @@ def extract_action_frames(video_path: str = '../../videos', annotation_path: str
             convert_frames_to_videos(video, frame_start, frame_end, case_folder, video_name)
             if j == len(labels) - 1 and last_frame <= total_frames - 1:
                 frame_end = total_frames - 1
-                video_name = f"{seg_video}/{case_list[i]}_idle_{last_frame}-{frame_end}.mp4"
+                video_name = f"{seg_video}/{case_list[i]}_Idle_{last_frame}-{frame_end}.mp4"
                 convert_frames_to_videos(video, last_frame, frame_end, case_folder, video_name)
         outer.update(1)
 
